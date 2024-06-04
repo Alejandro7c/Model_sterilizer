6983efb69aa45289f0b71053d77df922eb9e92a1
Alejandro7c <alex_merino@hotmail.com>
2024-06-04 14:00:35 +0200

    Update version.el

--- sources/version.el
+++ sources/version.el
@@ -1,7 +1,7 @@
 -- Define the version, date, author and comments associated to this library
 #LIBRARY_INFO
-	#VERSION = "0.0"
-	#DATE = "19/02/2020"
-	#AUTHOR = ""
+	#VERSION = "1.0"
+	#DATE = "04/06/2024"
+	#AUTHOR = "Alejandro Merino Gómez"
 	#COMMENTS= "No comments"
 END #LIBRARY_INFO
